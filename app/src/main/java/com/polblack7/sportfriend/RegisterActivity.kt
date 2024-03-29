0a9dfffc3b200d834ef5a573a5322f975a016e74
Nikita Annenkov <niannenkov@edu.hse.ru>
2024-03-29 20:44:46 +0300

    Fixed buttons, added login and fixed registration

--- app/src/main/java/com/polblack7/sportfriend/RegisterActivity.kt
+++ app/src/main/java/com/polblack7/sportfriend/RegisterActivity.kt
@@ -24,11 +24,7 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
         firebaseAuth = FirebaseAuth.getInstance()
 
         progressDialog = ProgressDialog(this)
@@ -41,7 +37,7 @@ class RegisterActivity : AppCompatActivity() {
         }
 
         binding.buttonRegister.setOnClickListener {
-
+            validateData()
         }
     }
     private var login = ""
