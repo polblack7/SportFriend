e0a4f7b5ee2902027c265e8c1eff13d0f22db582
Nikita Annenkov <niannenkov@edu.hse.ru>
2024-03-30 02:14:39 +0300

    finished login and admin dashboard

--- app/src/main/java/com/polblack7/sportfriend/AdminDashboardActivity.kt
+++ app/src/main/java/com/polblack7/sportfriend/AdminDashboardActivity.kt
@@ -1,20 +1,39 @@
 package com.polblack7.sportfriend
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.polblack7.sportfriend.databinding.ActivityAdminDashboardBinding
 
 class AdminDashboardActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAdminDashboardBinding
+
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_admin_dashboard)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityAdminDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+        binding.logOut.setOnClickListener {
+            firebaseAuth.signOut()
+            checkUser()
+        }
+    }
+
+    private fun checkUser() {
+        val firebaseUser =firebaseAuth.currentUser
+        if (firebaseUser == null) {
+            startActivity(Intent(this@AdminDashboardActivity, MainActivity::class.java))
+            finish()
+        } else {
+
         }
     }
 }
